bdc262e46e7411508a5be5586ce1d81aad5ba446
surajmlhub <surajmlhub@gmail.com>
2024-02-07 20:52:31 +0530

    changes in code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   standalone: true,
   imports: [],
-  template: `<h1>Hi ! Suraj Here</h1>`,
+  template: `<h1>Hi! Suraj Here</h1>`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
